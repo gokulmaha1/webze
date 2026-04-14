c6ae0f9e51619b9e0f30b38945ad18e3347b0869
gokulmaha1 <gokulmaha@gmail.com>
2026-04-14 19:57:37 +0530

    fix: Add missing websites relationship to Template model for Distribution Chart

--- saas/app/Models/Template.php
+++ saas/app/Models/Template.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Template extends Model
 {
     protected $guarded = [];
+
+    public function websites()
+    {
+        return $this->hasMany(Website::class);
+    }
 }
